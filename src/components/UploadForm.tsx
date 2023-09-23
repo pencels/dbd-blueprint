282fdfe6ce18070dba62790771b167e8b5991b94
Christopher Pence <cpence@ucsd.edu>
2023-09-23 04:02:42 -0700

    Minor styling changes

--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -167,7 +167,7 @@ export default function UploadForm() {
           accept=".zip"
           name="file"
           id="filepicker"
-          class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 file:!bg-gray-500 file:hover:!bg-gray-600"
           onInput={(e) => {
             setFile(e.currentTarget.files?.[0]);
           }}
